30cbc4e097c52c90f3b11a898481d38cec523b63
Adalberto Nivar <adalberto.nivar@gmail.com>
2017-03-09 15:58:04 -0400

    Front End

--- Biblioteca_eCommerce/Biblioteca_eCommerce/Controllers/HomeController.cs
+++ Biblioteca_eCommerce/Biblioteca_eCommerce/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Biblioteca_eCommerce.Models;
+using System.Data.Entity;
 
 namespace Biblioteca_eCommerce.Controllers
 {
     public class HomeController : Controller
     {
+        private BibliotecaDbContext db = new BibliotecaDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            var libros = db.Libros.Include(l => l.autores).Include(l => l.Bibliografia).Include(l => l.Editora);
+            return View(libros.ToList());
         }
 
         public ActionResult About()
